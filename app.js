6fd498253a4c40de536b85111d7989feb24e5837
Jimmy Jimenez <jimmyjimenez24000@gmail.com>
2026-07-30 00:40:51 -0400

    Add getHumanChoicefunction

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 function getComputerChoice() {
-  let choices = ['rock', 'paper', 'scissor'];
+  let choices = ['rock', 'paper', 'scissors'];
   let pick = Math.floor(Math.random() * 3);
   return choices[pick];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(userChoice) {
+  let choices = ['rock', 'paper', 'scissors'];
+  if (!choices.includes(userChoice.toLowerCase())) {
+    return 'Pick either rock, paper, or scissors';
+  }
+
+  return userChoice.toLowerCase();
+}
+
+console.log(getHumanChoice('IGLO'));
+console.log(getHumanChoice('rock'));
+console.log(getHumanChoice('PAPER'));
+console.log(getHumanChoice('scissors'));
