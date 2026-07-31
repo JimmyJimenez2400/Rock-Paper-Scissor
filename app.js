fce2f09bee40804e1efac69ec86a5c2cf64270f1
Jimmy Jimenez <jimmyjimenez24000@gmail.com>
2026-07-30 21:40:59 -0400

    Add playRound function to game logic

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
   let choices = ['rock', 'paper', 'scissors'];
-  let pick = Math.floor(Math.random() * 3);
-  return choices[pick];
+  let choice = Math.floor(Math.random() * 3);
+  return choices[choice];
 }
 
 function getHumanChoice(userChoice) {
@@ -13,6 +13,38 @@ function getHumanChoice(userChoice) {
   return userChoice.toLowerCase();
 }
 
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice.toLowerCase() === computerChoice) {
+    return `It's a tie!`;
+  }
+
+  if (humanChoice.toLowerCase() === 'rock' && computerChoice === 'scissors') {
+    humanScore++;
+    return `Player wins! ${humanChoice} beats ${computerChoice}`;
+  } else if (
+    humanChoice.toLowerCase() === 'paper' &&
+    computerChoice === 'rock'
+  ) {
+    humanScore++;
+    return `Player wins! ${humanChoice} beats ${computerChoice}`;
+  } else if (
+    humanChoice.toLowerCase() === 'scissors' &&
+    computerChoice === 'paper'
+  ) {
+    humanScore++;
+    return `Player wins! ${humanChoice} beats ${computerChoice}`;
+  } else {
+    computerScore++;
+    return `You lose! ${computerChoice} beats ${humanChoice}`;
+  }
+}
+
 // Global player scores
 let humanScore = 0;
 let computerScore = 0;
+
+const humanSelection = getHumanChoice('scissors');
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
